7d412aa20d71ce78763402bb8795e5aa621ea336
SalimmReza <refath300@gmail.com>
2021-12-19 22:24:55 +0600

    register and login

---
+++ app/src/main/java/com/example/personalbudgetingapp/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.personalbudgetingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
